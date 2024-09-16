e39b76f6bd18a50ef3e00822a5ddffd052bafd5b
Rahat Islam Akash <rahatislamakash@gmail.com>
2024-09-16 16:23:36 +0600

    all films repository updated

---
+++ lib/data/repositories/all_flims_repository/all_films_repository.dart
@@ -0,0 +1,9 @@
+// ignore_for_file: public_member_api_docs, sort_constructors_first
+import 'package:graphql_flutter/data/services/graphql_service.dart';
+
+class AllFilmsRepository {
+  final GraphqlService _graphqlService;
+  AllFilmsRepository(
+    this._graphqlService,
+  );
+}
