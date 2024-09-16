c5a92341cb413f5012bdf896bf0ad963925446fe
Rahat Islam Akash <rahatislamakash@gmail.com>
2024-09-16 21:30:49 +0600

    home_page file moved to pages diectory

--- lib/main.dart
+++ lib/main.dart
@@ -5,6 +5,7 @@ import 'package:get/route_manager.dart';
 import 'package:graphql_flutter/presentation/controllers/all_films_controller/all_films_controller.dart';
 
 import 'presentation/bindings/all_flims_binding/all_flims_binding.dart';
+import 'presentation/pages/home_page/home_page.dart';
 
 void main() {
   runApp(const MyApp());
@@ -25,43 +26,5 @@ class MyApp extends StatelessWidget {
       initialBinding: AllFlimsBinding(),
       home: const MyHomePage(title: 'GraphQL'),
     );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  final AllFilmsController controller = Get.find();
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-        appBar: AppBar(
-          title: Text(widget.title),
-        ),
-        body: Obx(() {
-          if (controller.loading.value) {
-            return const Center(
-              child: CircularProgressIndicator(),
-            );
-          }
-
-          return ListView.builder(
-            itemCount:
-                controller.allFilmsList.value.data!.allFilms!.films!.length,
-            itemBuilder: (context, index) {
-              return ListTile(
-                  title: Text(controller.allFilmsList.value.data!.allFilms!
-                          .films![index].title ??
-                      'title'));
-            },
-          );
-        }));
   }
 }
---
+++ lib/presentation/pages/home_page/home_page.dart
@@ -0,0 +1,42 @@
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+
+import '../../controllers/all_films_controller/all_films_controller.dart';
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key, required this.title});
+
+  final String title;
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  final AllFilmsController controller = Get.find();
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          title: Text(widget.title),
+        ),
+        body: Obx(() {
+          if (controller.loading.value) {
+            return const Center(
+              child: CircularProgressIndicator(),
+            );
+          }
+
+          return ListView.builder(
+            itemCount:
+                controller.allFilmsList.value.data!.allFilms!.films!.length,
+            itemBuilder: (context, index) {
+              return ListTile(
+                  title: Text(controller.allFilmsList.value.data!.allFilms!
+                          .films![index].title ??
+                      'title'));
+            },
+          );
+        }));
+  }
+}
